7eb7007cd8561f288941f5dcc8b1d31b93bf36ee
khalafCsharp <khalafmajid93@gmail.com>
2026-02-25 11:18:10 +0300

    Time: 926 ms (5.92%) | Memory: 0B (100.00%) - LeetSync

---
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+    With Group_cte AS 
+    (
+        Select customer_number,
+            Count(order_number) As NumberOfOrders
+    From Orders
+    Group By customer_number
+    )
+
+    Select customer_number
+    From Group_cte
+    Where NumberOfOrders = (Select Max(NumberOfOrders) From Group_cte)
